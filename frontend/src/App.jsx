ae5afa9e519fb858ab6681c1a7c19263b3e33929
Shiva Rama Krishna Talla <shivaramakrishnatalla@gmail.com>
2025-02-27 04:39:45 -0500

    App.jsx
    added some description

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,8 @@ function App() {
     return (
         <div style={{ textAlign: "center", marginTop: "50px" }}>
             <h1>React + Node.js CI/CD Project</h1>
-            <h2>{message}</h2>
+            <h2>As a Master's student in Computer Science, I built a CI/CD pipeline to automate the deployment of a React frontend (AWS S3 + CloudFront) and a Node.js backend (AWS Elastic Beanstalk). Using GitHub Actions, the pipeline performs automated building, testing, security checks, and deployment for seamless delivery.</h2>
+            <h3>{message}</h3>
         </div>
     );
 }
